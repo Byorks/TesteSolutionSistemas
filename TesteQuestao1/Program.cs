a691496a028b83d4899ca82c720ff8bb71e56608
Vanessa Byork <143918490+Byorks@users.noreply.github.com>
2025-09-16 10:48:20 -0300

    :sparkles: feat: concluido questoes 1 e 3

---
+++ TesteQuestao1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+//1.Faça um algoritmo ou programa que leia a idade de uma pessoa expressa em anos,
+//meses e dias e escreva a idade dessa pessoa expressa apenas em dias.
+//Considerar ano com 365 dias e mês com 30 dias. 
+
+
+
+namespace TesteQuestao1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias = 0, meses = 0, anos = 0, diasDecorridos = 0;
+            Console.WriteLine("Digite sua idade expressa em anos, meses e dias");
+
+            Console.WriteLine("Digite os anos");
+            anos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os meses");
+            meses = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite os dias");
+            dias = int.Parse(Console.ReadLine());
+
+            diasDecorridos += anos * 365;
+
+            diasDecorridos += meses * 30;
+
+            diasDecorridos += dias;
+
+            Console.WriteLine($"Já decorreram {diasDecorridos} dias");
+        }
+    }
+}
---
+++ TesteQuestão3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+//Escrever um algoritmo/programa que leia o número de carros por ele vendidos,
+//o valor total de suas vendas, o salário fixo e o valor que ele recebe por carro vendido.
+//Calcule e escreva o salário final do vendedor. 
+
+namespace TesteQuestão3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int carrosVendidos = 0;
+            double totalVendas = 0, salarioFixo = 0, comissaoCarros = 0, salarioFinal = 0;
+
+            Console.WriteLine("Insira o número total de carros vendidos");
+            carrosVendidos = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Insira o valor total de vendas");
+            totalVendas = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Insira o valor do salário fixo");
+            salarioFixo = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Insira o valor da comissão por carro vendido");
+            comissaoCarros = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            salarioFinal = carrosVendidos * comissaoCarros + totalVendas * .05 + salarioFixo; 
+
+            Console.WriteLine("Salário final = " + salarioFinal.ToString("F2", CultureInfo.InvariantCulture));
+
+        }
+    }
+}
